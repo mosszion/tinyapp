6889333bcf7b62fc90ea569fc2da8418764e1580
Mussie <musdzion@gmail.com>
2024-03-28 15:52:40 -0400

    Add HTML code into the server code

--- express_server.js
+++ express_server.js
@@ -21,6 +21,11 @@ app.get("/urls.json", (req,res) => {
   res.json(urlDatabase);
 })
 
+// sending HTML content code 
+app.get("/hello", (req,res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+}); 
+
 //our app server start listening at port 8080
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
