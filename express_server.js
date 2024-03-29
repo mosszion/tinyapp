f78c10b048442ca108af98b53439470f260721f6
Mussie <musdzion@gmail.com>
2024-03-29 03:26:43 -0400

    second route added to the code

--- express_server.js
+++ express_server.js
@@ -18,10 +18,17 @@ app.get("/",(req,res) => {
   res.send("Hello!");
 });
 
-//
+// adding first route
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+// adding second route
+app.get("/urls/:id", (req, res) => {
+  // const longURL = urlDatabase[req.params.id];
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
 });
 
 //additional endpoints for api usage
