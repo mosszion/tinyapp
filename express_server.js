2a6c6dc8e78abd07ce9f009b31b87d3c9807cf48
Mussie <musdzion@gmail.com>
2024-03-29 12:46:38 -0400

    minor corrections

--- express_server.js
+++ express_server.js
@@ -56,7 +56,7 @@ app.post("/urls", (req,res) => {
   urlDatabase[id] = longURL;
 
   //now redirect page to /urls/:id. 
-  res.redirect(`/u/:${id}`);
+  res.redirect(`/urls/:${id}`);
 })
 
 app.get("/u/:id", (req, res) => {
