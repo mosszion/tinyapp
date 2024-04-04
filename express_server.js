6d34f61f89d8944ba4ae8fae49f380ce26fdd209
Mussie <musdzion@gmail.com>
2024-04-04 15:55:43 -0400

    added users Database

--- express_server.js
+++ express_server.js
@@ -20,6 +20,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+/////////////////////////////////////////////////////////////
+///OUR URL Temporary USERS Database
+/////////////////////////////////////////////////////////////
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  }
+  
+};
+
 
 /////////////////////////////////////////////////////////////
 /// Aiding Function which generates random ID
@@ -29,6 +47,8 @@ function generateRandomString() {
   return randomid; //
   
 };
+
+
 /////////////////////////////////////////////////////////////
 ///Get route renders Registration page
 /////////////////////////////////////////////////////////////
