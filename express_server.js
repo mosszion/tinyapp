4293c6829f911bc0927e9e67548892e210707b27
Mussie <musdzion@gmail.com>
2024-04-04 05:51:14 -0400

    not working file

--- express_server.js
+++ express_server.js
@@ -1,114 +1,123 @@
-//get the express library
-const express = require("express");
-
-// assign express variable and port number
+/////////////////////////////////////////////////////////////
+///Setup files and dependancies plus libraries
+/////////////////////////////////////////////////////////////
+const express = require("express")
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
-
-//add middleware urlencoded 
+app.set("view engine", "ejs")
 app.use(express.urlencoded({extended: true}));
 
-//random short URL id generator function
-function generateRandomString() {
-  const randomid = Math.random().toString(36).substring(2, 8); //gets randomly generated 6 characters and assign it to randomID
-  return randomid; //
+/////////////////////////////////////////////////////////////
+///OUR URL Temporary Database
+/////////////////////////////////////////////////////////////
 
-}
-
-
-// an object with short keys to long url values
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-// when a request is made , responde with Hello!
+
+/////////////////////////////////////////////////////////////
+/// Aiding Function which generates random ID
+/////////////////////////////////////////////////////////////
+function generateRandomString() {
+  const randomid = Math.random().toString(36).substring(2, 8); //gets randomly generated 6 characters and assign it to randomID
+  return randomid; //
+  
+};
+/////////////////////////////////////////////////////////////
+///First route Hello Page 
+/////////////////////////////////////////////////////////////
+
 app.get("/",(req,res) => {
-  res.send("Hello!");
+  res.send("Hello!!");
+  
+});
+/////////////////////////////////////////////////////////////
+///Route to diplay all our urls 
+/////////////////////////////////////////////////////////////
+app.get("/urls",(req,res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index",templateVars);
+  
 });
 
-// adding /urls route
-// creates a new variable and assignes the urlDatabase object to it
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-//adding /urls/new route
-app.get("/urls/new", (req,res) => {
+/////////////////////////////////////////////////////////////
+///Get Route to show the new Form
+/////////////////////////////////////////////////////////////
+app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-})
-
-
-//adding post route
-app.post("/urls", (req,res) => {
-
-  //getting the longURL from the post
-  if (!req.body.longURL){
-    res.status(400).send("Please Enter Valid URL!!!!") 
-  }
-  const longURL = req.body.longURL
-
-  //generate and assign the random id to th longURL
-  const id = generateRandomString();
-   
-  //update our urlDatabase
-
-  urlDatabase[id] = longURL;
-
-  //now redirect page to /urls/:id. 
-  res.redirect(`/urls/${id}`);
-})
-
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
-  res.render("urls_show", templateVars);
 });
 
-
-
-//route which deletes if a delete button request is submitted
+/////////////////////////////////////////////////////////////
+/// Post Route that edits URL resource
+/////////////////////////////////////////////////////////////
+app.post("/urls/:id",(req,res) => {
+  const id = req.params.id;
+  const longURL = req.body.longURL
+  urlDatabase[id] = longURL;
+ 
+ res.redirect(`/urls`);
+});
+/////////////////////////////////////////////////////////////
+/// Post Route that removes URL resource
+/////////////////////////////////////////////////////////////
 app.post("/urls/:id/delete",(req,res) => {
   const id = req.params.id;
   delete urlDatabase[id];
-  res.redirect("/urls");
-  
-})
-//route which updates database if an edit button request is submitted
-app.post("/urls/:id",(req,res) => {
-  const id = req.params.id;
-  const newLongUrl = req.body.edit;
-  urlDatabase[id] = newLongUrl;
-  
-  res.redirect("/urls");
-  
-})
-// adding /urls/:id route
+ 
+ res.redirect("/urls");
+});
+/////////////////////////////////////////////////////////////
+/// POST Route to recieve the Form Submission
+/////////////////////////////////////////////////////////////
+
+app.post("/urls", (req, res) => {
+  const id = generateRandomString();
+  const longUrl = req.body.longURL
+  urlDatabase[id] =longUrl;
+
+  res.redirect(`/urls/${id}`);
+});
+/////////////////////////////////////////////////////////////
+/// Redirect Short URLs
+/////////////////////////////////////////////////////////////
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
-// creates a new templateVars which holds Id from the user 
-// ...plus value of the id
 
-//additional endpoints for api usage
+
+
+/////////////////////////////////////////////////////////////
+///Route to diplay a single URL and its shortened form 
+/////////////////////////////////////////////////////////////
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+
+/////////////////////////////////////////////////////////////
+/// Route for JSON data
+/////////////////////////////////////////////////////////////
+
 app.get("/urls.json", (req,res) => {
   res.json(urlDatabase);
 })
 
-
-// sending HTML content code 
+/////////////////////////////////////////////////////////////
+/// Sending HTML in a route
+/////////////////////////////////////////////////////////////
 app.get("/hello", (req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-}); 
-
-//our app server start listening at port 8080
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 })
 
-
+/////////////////////////////////////////////////////////////
+/// App listening at port 8080!
+/////////////////////////////////////////////////////////////
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
