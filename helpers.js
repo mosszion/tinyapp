89a67d44d45193bf2e973a4a7d08ea068b7abdcb
Mussie <musdzion@gmail.com>
2024-04-10 13:32:29 -0400

    added functions and exported to main server

--- helpers.js
+++ helpers.js
@@ -3,14 +3,38 @@
 
 function getUserByEmail(email,Database){
   
-  for( let user in Database ) {
+  for( let user in userDatabase ) {
     if(email === Database[user]["email"]) {
-        return user
+        return Database[user]
     }
   }
   return null;
+
+
 }
 
+//function which checks given email and password if correct and found in database
+function getUserByEmailByPassword(emailFromPost,passwordFromPost){
+  
+  for( let user in users ) {
+   
+    if (bcrypt.compareSync(passwordFromPost, users[user]["password"])&& (emailFromPost === users[user]["email"])){
+    
+    return user
+
+      }
+  }
+  return null;
+}
+//Function which generates random Ids
+function generateRandomString() {
+  const randomid = Math.random().toString(36).substring(2, 8); //gets randomly generated 6 characters and assign it to randomID
+  return randomid; //
+  
+};
+
 module.exports = {
-  getUserByEmail
+  getUserByEmail,
+  getUserByEmailByPassword,
+  generateRandomString
 }
